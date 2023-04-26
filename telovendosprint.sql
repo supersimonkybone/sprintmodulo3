c9338622b4a54cb2b118cd050c05a0ed05f3d0d8
MauricioJMoralesV <129233232+MauricioJMoralesV@users.noreply.github.com>
2023-04-25 22:14:45 -0400

    main actualizado con los 10 clientes y 5 proveedores

--- telovendosprint.sql
+++ telovendosprint.sql
@@ -39,9 +39,27 @@ GRANT ALL PRIVILEGES ON Sprint_telovendo.* TO 'admintienda'@'localhost';
 
 -- TABLA PRODUCTOS INFO: 
 -- 10 CLIENTES
--- 20 PRODUCTOS
--- 05 PROVEEDORES
+INSERT INTO cliente (nombres, apellidos, telefono, direccion, comuna) 
+VALUES 	('Ignacio Simon', 'Perez Lastra', '+56912345678', 'San Pablo 123', 'Vitacura'),
+		('Eduardo Ricardo', 'Rockefeler Soto', '+56943245678', 'San Pedro 321', 'Las Condes'),
+        ('Harry Edgard', 'Potter Rodriguez', '+56912654678', 'Providencia 23', 'Providencia'),
+        ('Hermione Sandra', 'Granger Sotomayor', '+56912345123', 'Alvarez 666', 'Viña del mar'),
+        ('Ron Vodka', 'Weasly Ramirez', '+56965435678', 'Limache 65', 'Viña del mar'),
+        ('Albus Peter', 'Dumbledore Lastra', '+56912765438', 'Londres 43', 'Peñablanca'),
+        ('Sasha Ignacia', 'Grey Perez', '+56918765678', 'Liverpool 89', 'Temuco'),
+        ('Severus Adolfo', 'Snape Pinochet', '+56917655678', 'Valparaiso 1314', 'Santiago'),
+        ('Minerva Marcela', 'McGonagall Veas', '+56976895678', 'Blanco Encalada 154', 'Quilpue'),
+        ('Homero Jay', 'Simpson Simpson', '+56979685678', 'Avenida Siempreviva 742', 'Springfield');
 
+-- 20 PRODUCTOS
+
+-- 05 PROVEEDORES
+INSERT INTO proveedor (representante_legal, nombre_corporativo, telefono, nombre_contacto, categoria, correo_facturacion) 
+VALUES 	('Rodrigo Monckenberg', 'Importaciones y Exportaciones Tecnodata S.A.', '+56224456350', 'Tecnodata', 'Tecnología', 'sertec@tecnodatasa.cl'),
+		('Ricardo Eluchans', 'Computación e ingeniería S.A.', '+56224112472', 'COINSA', 'Tecnología', 'facturacion@coinsa.cl'),
+		('Elena Risopatron', 'A&S Tecnologias Computacionales Ltda.', '+56226322930', 'A&S', 'e-commerce', 'pagofactura@aystecnologia.cl'),
+        ('Ignacia Piñera', 'ESPEX Ingeniería LTDA', '+56225124305', 'ESPEX', 'Servicios de Consultoría Técnica', 'facpag@espex.cl'),
+        ('James Wu', 'Foxconn Technology Co Ltd', '+886222683466', 'Foxconn', 'Tecnología', 'ir@foxconn.com');
 
 -- PREGUNTAS PARA EL CODIGO SPRINT
 -- 1.-  Cuál es la categoría de productos que más se repite.
