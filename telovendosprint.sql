03159e4347f2bb959bf6ed01fd4bf503258d71f1
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-04-26 21:16:03 -0400

    Update telovendosprint.sql

--- telovendosprint.sql
+++ telovendosprint.sql
@@ -41,7 +41,7 @@ GRANT ALL PRIVILEGES ON Sprint_telovendo.* TO 'admintienda'@'localhost';
 -- 10 CLIENTES
 INSERT INTO cliente (nombres, apellidos, telefono, direccion, comuna) 
 VALUES 	('Ignacio Simon', 'Perez Lastra', '+56912345678', 'San Pablo 123', 'Vitacura'),
-		('Eduardo Ricardo', 'Rockefeler Soto', '+56943245678', 'San Pedro 321', 'Las Condes'),
+	('Eduardo Ricardo', 'Rockefeler Soto', '+56943245678', 'San Pedro 321', 'Las Condes'),
         ('Harry Edgard', 'Potter Rodriguez', '+56912654678', 'Providencia 23', 'Providencia'),
         ('Hermione Sandra', 'Granger Sotomayor', '+56912345123', 'Alvarez 666', 'Viña del mar'),
         ('Ron Vodka', 'Weasly Ramirez', '+56965435678', 'Limache 65', 'Viña del mar'),
@@ -56,8 +56,8 @@ VALUES 	('Ignacio Simon', 'Perez Lastra', '+56912345678', 'San Pablo 123', 'Vita
 -- 05 PROVEEDORES
 INSERT INTO proveedor (representante_legal, nombre_corporativo, telefono, nombre_contacto, categoria, correo_facturacion) 
 VALUES 	('Rodrigo Monckenberg', 'Importaciones y Exportaciones Tecnodata S.A.', '+56224456350', 'Tecnodata', 'Tecnología', 'sertec@tecnodatasa.cl'),
-		('Ricardo Eluchans', 'Computación e ingeniería S.A.', '+56224112472', 'COINSA', 'Tecnología', 'facturacion@coinsa.cl'),
-		('Elena Risopatron', 'A&S Tecnologias Computacionales Ltda.', '+56226322930', 'A&S', 'e-commerce', 'pagofactura@aystecnologia.cl'),
+	('Ricardo Eluchans', 'Computación e ingeniería S.A.', '+56224112472', 'COINSA', 'Tecnología', 'facturacion@coinsa.cl'),
+	('Elena Risopatron', 'A&S Tecnologias Computacionales Ltda.', '+56226322930', 'A&S', 'e-commerce', 'pagofactura@aystecnologia.cl'),
         ('Ignacia Piñera', 'ESPEX Ingeniería LTDA', '+56225124305', 'ESPEX', 'Servicios de Consultoría Técnica', 'facpag@espex.cl'),
         ('James Wu', 'Foxconn Technology Co Ltd', '+886222683466', 'Foxconn', 'Tecnología', 'ir@foxconn.com');
 
